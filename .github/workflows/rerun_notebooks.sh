afeb6d359c023dd60084263ebdd3ad96cd155b57
Benoit Fanchon <benoit.fanchon@quandela.com>
2026-06-03 12:36:40 +0200

    Notebook patch debug

--- .github/workflows/rerun_notebooks.sh
+++ .github/workflows/rerun_notebooks.sh
@@ -10,6 +10,9 @@ run_notebook () {
 # apply patch to notebooks which allows execution
 SCRIPT_DIR=$(dirname "$(realpath $0)")
 GIT_ROOT_DIR=$(git rev-parse --show-toplevel)
+echo SCRIPT_DIR: ${SCRIPT_DIR}
+echo GIT_ROOT_DIR: ${GIT_ROOT_DIR}
+echo ENABLE_CLOUD_NOTEBOOKS: ${ENABLE_CLOUD_NOTEBOOKS}
 (cd $GIT_ROOT_DIR && git apply "$SCRIPT_DIR/notebooks.patch")
 
 nb_dir="docs/source/notebooks"
@@ -17,7 +20,7 @@ for entry in `ls $nb_dir | grep \.ipynb`; do
     notebook=$nb_dir/$entry
     if [[ $notebook =~ ".ipynb" ]]
     then
-        if [ $ENABLE_CLOUD_NOTEBOOKS = "True" ]
+        if [ "$ENABLE_CLOUD_NOTEBOOKS" = "True" ]
         then
             run_notebook
         elif [ "$notebook" = "docs/source/notebooks/Gedik_qudit.ipynb" ]
