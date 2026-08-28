453a106582ff2d9366da671dc3bf9b1c0fe5e544
Aubaert <109159219+Aubaert@users.noreply.github.com>
2026-08-28 16:27:42 +0200

    PCVL-1274 job group for emt jobs (#782)

--- perceval/runtime/execution.py
+++ perceval/runtime/execution.py
@@ -86,6 +86,14 @@ class Execution:
         return None
 
     @property
+    def computation(self) -> Computation | ComputationIterator:
+        return self._computation
+
+    @property
+    def computer(self) -> AbstractComputer:
+        return self._computer
+
+    @property
     def name(self) -> str:
         """
         The job name
@@ -99,16 +107,16 @@ class Execution:
         self._name = new_name
 
     @property
-    def job_group_name(self) -> str:
+    def job_group_name(self) -> str | None:
         """
-        The job name
+        The job group name
         """
-        return self._name
+        return self._job_group_name
 
     @job_group_name.setter
     def job_group_name(self, new_name: str):
-        if not isinstance(new_name, str):
-            raise TypeError("A job name must be a string")
+        if not isinstance(new_name, str) or len(new_name) == 0:
+            raise TypeError("A job group name must be a non-empty string")
         self._job_group_name = new_name
 
     def set_job_group_name(self, new_name: str):  # TODO: legacy; remove ?
